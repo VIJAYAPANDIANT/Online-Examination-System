6afc7b65857c0893f08349254b77554700d7bc05
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-03-09 11:20:05 +0530

    main

--- backend/src/main/java/com/exam/proctor/repository/ExamRepository.java
+++ backend/src/main/java/com/exam/proctor/repository/ExamRepository.java
@@ -4,8 +4,6 @@ import com.exam.proctor.entity.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ExamRepository extends JpaRepository<Exam, Long> {
 }
